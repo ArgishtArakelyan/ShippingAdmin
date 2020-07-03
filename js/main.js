f30f289d65518962ade7296b4905fc1b9fe75d91
Argish Arakelyan <argishtarakelyan@gmail.com>
2020-07-03 15:49:43 +0400

    ....

--- js/main.js
+++ js/main.js
@@ -43,3 +43,43 @@ $(document).on("click",".metismenu >li",function() {
 		$(this).find('.collapse').slideUp();
     }
 }); 
+
+
+ 
+  
+$(function() {
+    $('#datepicker').daterangepicker({
+      opens: 'left'
+    })
+  });
+
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+
+      type: 'line',
+
+      data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul' , 'Aug' , 'Sep' , 'Dec'],
+          datasets: [{
+              label: '',
+              backgroundColor: 'rgba(255, 255, 255, 0.1)',
+              borderColor: '#4c4c4c',
+              data: [0, 100, 150, 160, 150, 180, 250, 220 ,180 ,160],
+  
+          }]
+      },
+
+      options: {
+        legend: {
+            display: false
+        },
+        scales : {
+            xAxes : [ {
+                gridLines : {
+                    display : false
+                }
+            } ]
+        }
+      }
+  });
